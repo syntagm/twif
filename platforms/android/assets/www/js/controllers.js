48514c535cb1d7277c551cd9c9cfe1e40e71ba89
Syntagm <syntagm@icon.co.za>
2015-10-23 10:38:16 +0200

    Init1023_01
    the synch

---
+++ platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('fAQ&#x27;SCtrl', function($scope) {
+
+})
+   
+.controller('tWIFNewsCtrl', function($scope) {
+
+})
+   
+.controller('aboutTUE&#x27;SCtrl', function($scope) {
+
+})
+   
+.controller('linksCtrl', function($scope) {
+
+})
+ 
